a2fb2aa506b58d6aac3b76ebc60eea5c1dc660cb
ian <barkatthetree@gmail.com>
2015-09-22 23:32:05 +0800

    first commit

---
+++ C-Skin/AddViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C-Skin
+//
+//  Created by Ian Chen on 21/9/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddViewController : UIViewController
+
+
+@end
+
---
+++ C-Skin/AddViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  C-Skin
+//
+//  Created by Ian Chen on 21/9/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import "AddViewController.h"
+
+@interface AddViewController ()
+
+@end
+
+@implementation AddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
